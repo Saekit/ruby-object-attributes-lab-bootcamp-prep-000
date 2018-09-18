76ac628531125539e0053c991d0c27e99b02f53c
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-18 21:14:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,9 +3,9 @@ class Dog
     @name = name
   end
   
-  # def name
-  #   @name 
-  # end
+  def name
+    @name 
+  end
   
   def name=(new_name)
     @name = new_name
@@ -15,9 +15,9 @@ class Dog
     @breed = breed
   end
   
-  # def breed
-  #   @breed 
-  # end
+  def breed
+    @breed 
+  end
   
   def breed=(new_breed)
     @breed = new_breed
